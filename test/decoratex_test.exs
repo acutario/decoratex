003a0c6fa1a6b66ac48c936896ff428d7d90d81e
Ruben Sierra <maguilag@gmail.com>
2017-02-18 12:45:12 +0100

    Add tests

--- test/decoratex_test.exs
+++ test/decoratex_test.exs
@@ -2,7 +2,36 @@ defmodule DecoratexTest do
   use ExUnit.Case
   doctest Decoratex
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    test_model = %TestModel{}
+    {:ok, test_model: test_model}
+  end
+
+  test "decorate all fields", %{test_model: test_model} do
+    decorated_model = test_model
+    |> TestModel.decorate
+    assert decorated_model.module_name == TestModel.module_name(test_model)
+    assert decorated_model.module_length == TestModel.module_length(test_model)
+  end
+
+  test "decorate one field", %{test_model: test_model} do
+    decorated_model = test_model
+    |> TestModel.decorate(:module_name)
+    assert decorated_model.module_name == TestModel.module_name(test_model)
+    assert decorated_model.module_length == nil
+  end
+
+  test "decorate other field", %{test_model: test_model} do
+    decorated_model = test_model
+    |> TestModel.decorate(:module_length)
+    assert decorated_model.module_name == nil
+    assert decorated_model.module_length == TestModel.module_length(test_model)
+  end
+
+  test "decorate a list of fields", %{test_model: test_model} do
+    decorated_model = test_model
+    |> TestModel.decorate([:module_name, :module_length])
+    assert decorated_model.module_name == TestModel.module_name(test_model)
+    assert decorated_model.module_length == TestModel.module_length(test_model)
   end
 end
