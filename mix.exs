33d2bd8fe548b7963d4b63356cb3cc42c38309b4
Ruben Sierra <maguilag@gmail.com>
2017-02-18 12:38:45 +0100

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -27,6 +27,9 @@ defmodule Decoratex.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ecto, "~> 2.0"},
+     {:ex_doc, "~> 0.11", only: :dev},
+     {:earmark, "~> 0.1", only: :dev},
+     {:dialyxir, "~> 0.3", only: :dev}]
   end
 end
