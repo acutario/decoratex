66d790b7ab24fbcad65fab099afe006b84b863a3
Ruben Sierra <maguilag@gmail.com>
2018-02-18 17:28:45 +0100

    Update travis config

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Decoratex.Mixfile do
       {:ex_doc, "~> 0.11", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
       {:dialyxir, "~> 0.3", only: :dev},
-      {:credo, "~> 0.4", only: :dev}
+      {:credo, "~> 0.4", only: [:dev, :test]}
     ]
   end
 
