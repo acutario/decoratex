ff1f523f29356abd5cedf691627cd0536bf7fd0c
Junji Zhi <junji@hockeystick.co>
2019-03-06 16:33:40 -0500

    Bump ecto version to 3.0

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Decoratex.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ecto, "~> 2.0"},
+      {:ecto, "~> 3.0"},
       {:ex_doc, "~> 0.11", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
       {:dialyxir, "~> 0.3", only: :dev},
